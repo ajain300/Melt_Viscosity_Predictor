8aebed3de08323512296a6e4873cf9e31c605688
ajain300 <82125378+ajain300@users.noreply.github.com>
2021-11-17 11:18:05 -0500

    added NN and more data. more functionality to pre processing

--- gpflow_tools/mean_func.py
+++ gpflow_tools/mean_func.py
@@ -24,10 +24,10 @@ class shear_mean(mf.MeanFunction):
         if input_dim is None:
             raise ValueError(
                 "An input_dim needs to be specified when using the "
-                "`mol_weight` mean function in combination with expectations."
+                "`shear` mean function in combination with expectations."
             )
-        self.input_dim = input_dim - 3
-        A = np.concatenate((np.array([0,0,-0.6]), np.zeros(self.input_dim))).reshape(-1,1)
+        self.input_dim = input_dim - 1
+        A = np.concatenate((np.array([0,0,-0.6]), np.zeros(self.input_dim - 2))).reshape(-1,1)
         b = np.zeros((1))
         self.A = gpflow.Parameter(np.atleast_2d(A))
         self.b = gpflow.Parameter(b)
@@ -66,3 +66,19 @@ class weight_temp_mean(mf.MeanFunction):
     def __call__(self, X):
         temps = tf.math.add(tf.gather(X, self.temp, axis=1), 275.15)
         return tf.math.add(tf.math.scalar_mul(self.wM, tf.tensordot(X, self.A, [[-1], [0]]) + self.b), tf.math.scalar_mul(self.wT, tf.experimental.numpy.log10(tf.reshape(tf.math.exp(tf.math.scalar_mul(self.B, tf.math.reciprocal(temps))), [-1, 1]))))
+
+class weight_shear_mean(mf.MeanFunction):
+    def __init__(self, input_dim=None):
+        self.input_dim = input_dim - 1
+        As = np.concatenate((np.array([0,0,-0.6]), np.zeros(self.input_dim - 2))).reshape(-1,1)
+        b = np.zeros((1))
+        self.As = gpflow.Parameter(np.atleast_2d(As))
+        self.bs = gpflow.Parameter(b)
+        Aw = np.concatenate((np.array([3.4]), np.zeros(self.input_dim))).reshape(-1,1)
+        self.Am = gpflow.Parameter(np.atleast_2d(Aw))
+        self.bm = gpflow.Parameter(b)
+        self.wM = gpflow.Parameter(0.5)
+        self.wS = gpflow.Parameter(0.5)
+
+    def __call__(self, X):
+        return tf.math.add(tf.math.scalar_mul(self.wS, tf.tensordot(X, self.As, [[-1], [0]]) + self.bs), tf.math.scalar_mul(self.wM, tf.tensordot(X, self.Am, [[-1], [0]]) + self.bm))
